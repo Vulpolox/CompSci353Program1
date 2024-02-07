d2f9d59273d65a626de32fb45e4435ef54ac831c
Vulpolox <113536413+Vulpolox@users.noreply.github.com>
2024-02-07 13:33:13 -0600

    finished FileReader.rkt and started working on Main.rkt

--- FileReader.rkt
+++ FileReader.rkt
@@ -1,6 +1,11 @@
 #lang racket
 
-(define lines (file->lines "scores.txt"))           ; stores the contents of "scores.txt" as a list of the lines in it
-(define sub-strings (map split-line input-lines))   ; splits each line in lines into a white space delimited list of strings
+(provide data)
 
-sub-strings
+; pre  -- a line of text
+; post -- returns a list of strings where each string is obtained from the line by splitting it by whitespace
+(define (split-line input-string)
+  (string-split input-string))
+
+(define lines (file->lines "scores.txt"))     ; stores the contents of "scores.txt" as a list of the lines in it
+(define data (map split-line lines))          ; splits each line in lines into a white space delimited list of strings
--- Main.rkt
+++ Main.rkt
@@ -1,6 +1,25 @@
 #lang racket
 
 (require rackunit)
-(require "CalculateScore.rkt")
-(require "FileReader.rkt")
+(require "CalculateScore.rkt") ; provides "calculate-score" function for getting bowling scores
+(require "FileReader.rkt")     ; provides "data" which contains file contents
+
+(define player-hash (make-hash))
+
+; pre  -- takes a list
+; post -- returns a string indicating whether the input list refers to a team or a player
+(define (team-or-player? input-list)
+  (if (eq? (length input-list) 1)
+  "team"
+  "player"))
+
+; pre  -- takes a list pertaining to a player
+; post -- splits the name and the score into separate lists
+(define (player-score-split input-list)
+  (define player-name (list (first input-list)
+                            (second input-list)))
+  (define player-score (drop input-list 2))
+  (list player-name player-score))
+
+
   
