d10709b6e894a0f8b2a06277716dea1b2f81b53a
Vulpolox <113536413+Vulpolox@users.noreply.github.com>
2024-02-07 17:34:53 -0600

    made bad function

--- Main.rkt
+++ Main.rkt
@@ -4,7 +4,8 @@
 (require "CalculateScore.rkt") ; provides "calculate-score" function for getting bowling scores
 (require "FileReader.rkt")     ; provides "data" which contains file contents
 
-(define player-hash (make-hash))
+(define team->player-hash (make-immutable-hash))
+(define player->scores-has (make-hash))
 
 ; pre  -- takes a list
 ; post -- returns a string indicating whether the input list refers to a team or a player
@@ -15,7 +16,7 @@
 
 ; pre  -- takes a list pertaining to a player
 ; post -- returns a list containing the player name as a single string
-;         and a list of single strings representing said player's bowling game
+;         and a nested list of single strings representing said player's bowling game
 (define (player-score-split input-list)
   (define player-name (string-join (take input-list 2) " "))
   (define player-score (drop input-list 2))
@@ -34,7 +35,31 @@
   (define pure-char-list (map str->char input-list)) ; intermediary list that has strings converted to chars
   (map char->int-if-num pure-char-list))
 
-(define test (second data))
-(team-or-player? test)
-(player-score-split test)
-(calculate-score (type-cast-game (second (player-score-split test))))
+; pre  -- takes data from scores.txt
+; post -- hashes data into teams (keys) and lists of players (values) and returns map
+(define (hash-team->players data [out-hash (make-immutable-hash)] [current-team "null"])
+  (cond
+    [(empty? data)
+     out-hash]
+    [(eq? (team-or-player? (first data))
+          "team")
+     (hash-team->players (cdr data) out-hash (first data))]
+    [(eq? (team-or-player? (first data))
+          "player")
+     (begin
+       (define new-hash (hash-set out-hash
+                                  current-team
+                                  (first (player-score-split (first data)))))
+       (hash-team->players (cdr data) new-hash current-team))]
+    )
+  )
+     
+ (define team->players# (hash-team->players data))
+team->players#
+data
+
+
+;(define test (second data))
+;(team-or-player? test)
+;(player-score-split test)
+;(map team-or-player? data)
