28ee0ad4c2648c6e453bdc6a324360d541561fb2
Vulpolox <113536413+Vulpolox@users.noreply.github.com>
2024-02-07 11:06:38 -0600

    added a call to split-lines

--- FileReader.rkt
+++ FileReader.rkt
@@ -1,5 +1,6 @@
 #lang racket
 
-(define lines (file->lines "scores.txt"))
+(define lines (file->lines "scores.txt"))           ; stores the contents of "scores.txt" as a list of the lines in it
+(define sub-strings (map split-line input-lines))   ; splits each line in lines into a white space delimited list of strings
 
-
+sub-strings
