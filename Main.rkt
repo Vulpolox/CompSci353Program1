51a90e3ac600ec0c2310edb117c5fbf79397dab3
Vulpolox <jack.w.a.keys@gmail.com>
2024-02-08 06:38:47 -0600

    Added functions to help with desired output

--- Main.rkt
+++ Main.rkt
@@ -24,6 +24,18 @@
                      (filter team? data)))
   (set->list (list->set raw-lines)))
 
+; pre  -- takes a list
+; post -- returns a list containing all player names
+(define (get-player-names data)
+  (define (player? input-line)
+    (eq? "player" (team-or-player? input-line)))
+  (define (map-helper-func line)
+    (string-join (take line 2) " "))
+  (define raw-lines (map
+                     map-helper-func
+                     (filter player? data)))
+  (set->list (list->set raw-lines)))
+
 ; pre  -- takes a list pertaining to a player
 ; post -- returns a list containing the player name as a single string
 ;         and a nested list of single strings representing said player's bowling game
@@ -156,7 +168,51 @@
 
 (define clean-data (get-clean-data data))                ; data that is formatted nicely
 (define team-names (get-team-names data))                ; a list containing team names
+(define player-names (get-player-names data))            ; a list of all player names
 (define player2scores-map (player->score# clean-data))   ; a hash-map mapping players to a list of their 3 scores
 (define team2scores-map (team->score# clean-data))       ; a hash-map mapping teams to a list containing all scores from all games played by each one
 (define team2players-map (team->player# clean-data))     ; a hash-map mapping team names to a list players who belong to them
 
+; pre  -- takes a player name
+; post -- returns their 3 bowling scores as a string
+(define (get-scores player-name)
+  (define score-list (hash-ref player2scores-map
+                               player-name))
+  (string-join (map number->string score-list)
+               " "))
+
+; pre  -- takes a player name
+; post -- returns the sum of the scores of said player's 3 games
+(define (get-total-score player-name)
+  (foldr +
+         0
+         (hash-ref player2scores-map player-name)))
+
+; pre  -- takes a team name
+; post -- returns the total score for said team
+(define (total-team-score team-name)
+  (define player-list (hash-ref team2players-map
+                                team-name))
+  (foldr +
+         0
+         (map get-total-score
+              player-list)))
+
+; pre  -- takes a list of an arbitrary number of bowling team names
+; post -- returns the name of the team with the highest score
+(define (get-winning-team team-list)
+  (define highest-score (apply max (map total-team-score
+                                  team-list)))
+  (if (eq? (total-team-score (first team-list))
+           highest-score)
+      (first team-list)
+      (get-winning-team (cdr team-list))))
+
+; pre  -- takes a list of player names
+; post -- returns a list containing all the players who have the top score
+
+;------------------OUTPUT-------------------------------------------------------------------------------------------------------
+
+(map get-total-score player-names)
+         
+  
